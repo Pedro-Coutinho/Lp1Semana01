b970c09e3c448ef539ecfa95ee57d839abaa4d1c
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-03-03 11:05:20 +0000

    Add multiples types of string creation

--- VariasStrings/Program.cs
+++ VariasStrings/Program.cs
@@ -15,6 +15,31 @@ namespace VariasStrings
             Console.WriteLine(verbatimString1);
             Console.WriteLine(normalString2);
             Console.WriteLine(verbatimString2);
+
+            //Concatenação
+            int x = 1;
+            string conc1 = "abc" + x;
+            string conc2 = "abc" + 1;
+
+            //Interpolação
+            x = 7;
+            int y = 2;
+            string inter1 = $"Valor de x = {x}";
+            string inter2 = $"y = {y}, então y + x = {y+x}";
+
+            Console.WriteLine(inter1);
+            Console.WriteLine(inter2);
+
+            //String Formarting
+            string strF = String.Format("Y é {0}, X é {1}", y, x);
+            Console.WriteLine(strF);
+
+            //Direto
+            Console.WriteLine("This is a normal String with \"");
+            Console.WriteLine(@"This is a verbatim String with \""");
+            Console.WriteLine("This is a concatenated string + " + x);
+            Console.WriteLine($"This is a interpolated String, x = {x}");
+            Console.WriteLine(String.Format("This is a formated string, x = {0}, y = {1}",x,y));
         }
     }
 }
