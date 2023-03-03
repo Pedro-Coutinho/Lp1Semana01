0fc426bdeba056af5b3f5aadc59529bffb0d3452
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-03-03 10:24:56 +0000

    Add Xml comments

--- OlaMundo/Program.cs
+++ OlaMundo/Program.cs
@@ -2,8 +2,13 @@
 
 namespace OlaMundo
 {
+    
     class Program
     {
+        /// <summary>
+        /// Main Loop of program
+        /// </summary>
+        /// <param name="args">Arguments passed on console</param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
