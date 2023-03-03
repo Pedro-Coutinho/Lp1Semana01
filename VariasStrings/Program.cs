637ff7c09a919346e5c9a17174f4082f3b013e26
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-03-03 10:53:44 +0000

    Add VariasStrings to sln

---
+++ VariasStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace VariasStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string normalString1 = "This is a normal string with a new line \n";
+            string verbatimString1 = @"This is a verbatim string with a new line \n";
+            string normalString2 = "This is a normal string with a unicode \u2706";
+            string verbatimString2 = @"This is a verbatim string with a unicode \u2706";
+
+            Console.WriteLine(normalString1);
+            Console.WriteLine(verbatimString1);
+            Console.WriteLine(normalString2);
+            Console.WriteLine(verbatimString2);
+        }
+    }
+}
