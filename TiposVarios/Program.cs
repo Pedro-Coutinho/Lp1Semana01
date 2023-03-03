65ffeec2676a2dd2de3e922064573db72221f884
Pedro Coutinho <pedro_coutinho18@outlook.com>
2023-03-03 10:37:17 +0000

    Add TiposVarios sln

---
+++ TiposVarios/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace TiposVarios
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inteiros
+            int myInt = 1;
+            uint myUInt = 2;
+            long myLong = 3;
+            ulong myULong = 4;
+
+            Console.WriteLine(myInt);
+            Console.WriteLine(myUInt);
+            Console.WriteLine(myLong);
+            Console.WriteLine(myULong);
+        }
+    }
+}
